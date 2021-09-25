36ad029a57a593f82ec8ce259131b601a539da01
shuchi <shuchi.khandelwal@gmail.com>
2021-09-25 12:41:08 -0500

    new

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
 from flask import Flask, render_template, redirect,jsonify
 import pymongo
-import os
-import psycopg2
-from pymongo import MongoClient
-import socket
 
-
-db_name = "parks_db"
-
-#check if we're running in heroku and my environmental variable exist
-if 'DATABASE_URL' in os.environ:
-    mongo_url = os.environ['MONGO_URL']
-else:
-    #if we're not running in heroku then try and get my local config password
-    from db import config
-    mongo_url = "mongodb://localhost:27017/"
-
-
-app=Flask(__name__)
+app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
 conn = "mongodb://localhost:27017"
 
+<<<<<<< HEAD
 # #Pass connection to the pymongo instance
 client = pymongo.MongoClient(conn)
+=======
+parks_collection = "parks"
+visits_collection = "visits"
+activities_collection = "activities"
+>>>>>>> 094cb0d29fcc592f6a3e404fb111962fb57178f3
 
 # #connect to a database.
 db = client.parks_db
 collection = db.parks
 
 #print(db.parks.find_one())
+
+
+client = MongoClient(mongo_url)
+
+db = client[db_name]
 
 
 @app.route("/")
@@ -41,73 +36,114 @@ def index():
 @app.route("/name.html")
 def p_name():
     
+<<<<<<< HEAD
     results = collection.find()
-    #results is a cursor object, when looping through it each result is a dictionary
-    names_from_db = [result["fullName"] for result in results]
+=======
+   collection = db[parks_collection]
 
-    return render_template("name.html", names = names_from_db)
+   results = collection.find()  
+>>>>>>> 094cb0d29fcc592f6a3e404fb111962fb57178f3
+    #results is a cursor object, when looping through it each result is a dictionary
+   names_from_db = [result["fullName"] for result in results]
+
+   return render_template("name.html", names = names_from_db)
 
 
 @app.route("/parks/<pCode>")
 def park_detail(pCode):
+<<<<<<< HEAD
    
+=======
+
+    collection = db[parks_collection]
+
+>>>>>>> 094cb0d29fcc592f6a3e404fb111962fb57178f3
     park_info_from_db = collection.find({"parkCode": pCode})[0]
         
     # string_to_list(park_info_from_db,"images_url") 
     
-    return render_template("park_detail.html", park = park_info_from_db)
+    return render_template("park_detail_v2.html", park = park_info_from_db)
 
 
 @app.route("/api/v1/markers")
 def markers_api():
+<<<<<<< HEAD
    
     results = db.parks.find()
+=======
+
+    collection = db[parks_collection]
+
+    results = collection.find()
+>>>>>>> 094cb0d29fcc592f6a3e404fb111962fb57178f3
   
     data = [ {"latitude": result["latitude"], "longitude": result["longitude"], "parkCode" :result["parkCode"], "fullName" :result["fullName"], "designation" :result["designation"] , "states" :result["states"] } for result in results]
     return jsonify(data)
 
 
-def string_to_list(data, tag_name)  :
-    data[tag_name]= data[tag_name].replace("[", "").replace("]","").replace("'","").split(", ")
-
-
-@app.route("/park_detail.html")
-def park():
-    return render_template("park_detail.html")
+@app.route("/team.html")
+def team():
+    return render_template("team.html")
 
 
 @app.route("/visitation.html")
 def visitation():
+<<<<<<< HEAD
+=======
+
+    collection = db[visits_collection]
+
+>>>>>>> 094cb0d29fcc592f6a3e404fb111962fb57178f3
     results = collection.find()
     return render_template("visitation.html")
 
 
 @app.route("/api/v1/visits")
 def visit_api():
+<<<<<<< HEAD
    
-    results = db.visits.find({"Rank": {"$lt":30}})
+    results = db.visits.find()
+=======
+
+    collection = db[visits_collection]
+
+    results = collection.find()
+>>>>>>> 094cb0d29fcc592f6a3e404fb111962fb57178f3
   
-    data = [ {"park": result["ParkName"], "year": result["Year"], "visits" :result["Value"] } for result in results]
+   #data = [ {"park": result["ParkName"], "year": result["Year"], "visits" :result["Value"], "rank": result["Rank"]} for result in results]
+    data = []
+    for result in results:
+        park=next((item for item in data if item["park"] == result["ParkName"]), None)
+        if park == None :
+            park = {"park": result["ParkName"]}
+            data.append(park)
+
+        park[f'y{result["Year"]}'] = result["Value"]      
 
     print(data)
     return jsonify(data)
 
+@app.route("/api/v1/activities")
+<<<<<<< HEAD
+def activities_api():
 
-@app.route("/api/v1/activites")
+   
+    results = db.activities.find()
+  
+    data = [ {"count": result["Value"], "type": result["Type"],} for result in results]
+=======
 def activites_api():
-   
-    results = db.activites.find()
-  
-    data = [ {"count": result["Value"], "type": result["Type"] ,}for result in results]
 
+    collection = db[activities_collection]
+
+    results = collection.find()
+  
+    data = [ {"count": result["Value"], "type": result["Type"]} for result in results]
+
+>>>>>>> 094cb0d29fcc592f6a3e404fb111962fb57178f3
     print(data)
     return jsonify(data)
 
 
-
-
-if __name__ =="__main__":
- app.run(debug=True)
-   
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,44 +1,133 @@
-d3.json( location.origin + "/api/v1/visits").then((data) => {
 
+d3.json(location.origin + "/api/v1/visits").then((data) => {
 
-// Trace for parks visits
-let trace1 = {
-    x: data.filter(row=>row.year==2018).map(row => row.park),
-    y: data.filter(row=>row.year==2018).map(row => row.visits),
+   data = data.sort(function(a, b){return b.y2020 - a.y2020}).slice(0,20) 
+
+  // Trace for parks visits
+  let trace1 = {
+    x: data.map(row => row.park),
+    y: data.map(row => row.y2020),
     type: "bar",
-    
-  };
-let trace2 = {
-    x: data.filter(row=>row.year==2019).map(row => row.park),
-    y: data.filter(row=>row.year==2019).map(row => row.visits),
-    type: "bar",
-    
+    name: 2020
   };
 
- 
-// Data trace array
-let traceData = [trace1, trace2];
+  let trace2 = {
+    x: data.map(row => row.park),
+    y: data.map(row => row.y2019),
+    type: "bar",
+    name: 2019
+  };
 
-// Apply the group barmode to the layout
-let layout = {
-  title: {
-    text: "Most Visited Parks 2020 (in millions of visits)",
-    font: {
-      color:'#f0f0f0',
-   }
-  },
-  paper_bgcolor: '#232e35',
-  plot_bgcolor: '#232e35',
-  xaxis:{
-  color: '#f0f0f0'
-  },
-  yaxis:{
-    color: '#f0f0f0'
-  },
-  barmode: 'group'
+  let trace3 = {
+    x: data.map(row => row.park),
+    y: data.map(row => row.y2018),
+    type: "bar",
+    name: 2018
+  };
+  
+  
+  let trace4 = {
+    x: data.map(row => row.park),
+    y: data.map(row => row.y2017),
+    type: "bar",
+    name: 2017
+    };
+
+  // Data trace array
+  let traceData = [trace1, trace2, trace3, trace4];
+
+  // Apply the group barmode to the layout
+  let layout = {
+    title: {
+      text: "",
+      font: {
+        color: '#f0f0f0',
+      }
+    },
+    paper_bgcolor: '#232e35',
+    plot_bgcolor: '#232e35',
+    
+    xaxis: {
+      color: '#f0f0f0'
+    },
+    yaxis: {
+      color: '#f0f0f0'
+    },
+    legend: {
+      x: 1.0,
+      y: 1.0,
+      font: {
+        color: '#f0f0f0',
+        size : 12
+      }   
+
+    },
+    barmode: 'group'
+  };
+
+
+  // Render the plot to the div tag with id "plot"
+  Plotly.newPlot("plot", traceData, layout);
+});
+
+d3.json(location.origin + "/api/v1/activities").then((data) => {
+  data = data.sort(function(a, b){return b.count - a.count}) 
+  // sorted in descending order, first 20  
+  var data1 = data.slice(0,20)
+  let pieData = [
+    {
+        values: data1.map(row=>row.count),
+        labels: data1.map(row=>row.type),
+        hoverinfo: "hovertext",
+        type: "pie"
+    }
+  ];
+
+let pieLayout = {
+    margin: { t: 0, l: 0 },
+    paper_bgcolor: '#232e35',
+    plot_bgcolor: '#232e35',
+    legend: {
+      traceorder: 'normal',
+      font: {
+        size: 12,
+        color: '#f0f0f0',
+      },
+      bgcolor: '#232e35',
+      bordercolor: '#232e35',
+      borderwidth: 2
+    }
 };
 
+Plotly.plot("pie", pieData, pieLayout);
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", traceData, layout);
+  data = data.sort(function(a, b){return a.count - b.count}).slice(0,20) 
+  // sorted in ascending order, first 20  
+
+  let pieData1 = [
+    {
+        values: data.map(row=>row.count),
+        labels: data.map(row=>row.type),
+        hoverinfo: "hovertext",
+        type: "pie"
+    }
+];
+
+let pieLayout1 = {
+    margin: { t: 0, l: 0 },
+    paper_bgcolor: '#232e35',
+    plot_bgcolor: '#232e35',
+    legend: {
+      traceorder: 'normal',
+      font: {
+        size: 12,
+        color: '#f0f0f0',
+      },
+      bgcolor: '#232e35',
+      bordercolor: '#232e35',
+      borderwidth: 2
+    }
+};
+
+Plotly.plot("pie1", pieData1, pieLayout1);
 });
